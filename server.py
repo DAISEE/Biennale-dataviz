85a46c044f47b9a53378f3412956771d8d0badbd
Sam <sam@sraxrt.me>
2017-02-20 15:40:14 +0100

    use of leaflet with static data

--- server.py
+++ server.py
@@ -1,13 +1,23 @@
-from flask import Flask
-from flask import render_template
+#/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
+from flask import Flask, render_template
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/')
+def accueil():
+    return render_template('index.html')
+
+@app.route('/heatmap')
+def heatmap():
+    return render_template('heatmap.html')
+
+@app.route('/oeuvres')
+def oeuvres():
+    return render_template('oeuvres.html')
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+    app.run()
